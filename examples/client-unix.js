ec57a7ebd4ff1a8374ca5dfe1a33dba10c12608a
Peter Griess <pg@std.in>
2010-06-17 19:55:35 -0500

    Support sending fds; fire open event.

--- examples/client-unix.js
+++ examples/client-unix.js
@@ -6,3 +6,7 @@ var ws = new WebSocket('ws+unix://' + process.argv[2], 'boffo');
 ws.addListener('message', function(d) {
     sys.debug('Received message: ' + d.toString('utf8'));
 });
+
+ws.addListener('open', function() {
+    ws.send('This is a message', 1);
+});
--- examples/server-unix.js
+++ examples/server-unix.js
@@ -4,6 +4,10 @@ var ws = require('ws');
 var srv = ws.createServer({ debug : true});
 srv.addListener('connection', function(s) {
     sys.debug('Got a connection!');
+
+    s._req.socket.addListener('fd', function(fd) {
+        sys.debug('Got an fd: ' + fd);
+    });
 });
 
 srv.listen(process.argv[2]);
