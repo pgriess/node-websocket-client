029474e84555c0c59b8c9b318b0c83755cd6d568
Peter Griess <pg@std.in>
2010-06-18 09:44:08 -0500

    Add s->c message to unit test.

--- test/test-basic.js
+++ test/test-basic.js
@@ -4,15 +4,19 @@ var WebSocket = require('websocket').WebSocket;
 var WebSocketServer = require('ws').Server;
 
 var PORT = 1024 + Math.floor(Math.random() * 4096);
+var MSG = 'This is a test: ' + (Math.random() * 100);
 
 var gotServerConnection = false;
 var gotServerClose = false;
-var gotOpen = true;
+var gotClientOpen = true;
+var gotClientData = true;
 
 var wss = new WebSocketServer();
 wss.listen(PORT, 'localhost');
 wss.addListener('connection', function(c) {
     gotServerConnection = true;
+
+    c.write(MSG);
 
     c.addListener('close', function() {
         gotServerClose = true;
@@ -22,12 +26,18 @@ wss.addListener('connection', function(c) {
 
 var ws = new WebSocket('ws://localhost:' + PORT + '/', 'biff');
 ws.addListener('open', function() {
-    gotOpen = true;
+    gotClientOpen = true;
+});
+ws.addListener('data', function(buf) {
+    gotClientData = true;
+    assert.equal(buf.toString('utf8'), MSG);
+
     ws.close();
 });
 
 process.addListener('exit', function() {
     assert.ok(gotServerConnection);
-    assert.ok(gotOpen);
+    assert.ok(gotClientOpen);
+    assert.ok(gotClientData);
     assert.ok(gotServerClose);
 });
