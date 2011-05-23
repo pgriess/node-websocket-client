bfb245ccfb755c273dddd62e4acbe5bbba98faed
indexzero <charlie.robbins@gmail.com>
2011-05-23 03:11:10 -0400

    [test] Update require statements to websocket-server

--- test/test-readonly-attrs.js
+++ test/test-readonly-attrs.js
@@ -3,7 +3,7 @@
 var assert = require('assert');
 var sys = require('sys');
 var WebSocket = require('../lib/websocket').WebSocket;
-var WebSocketServer = require('websocket-server/ws/server').Server;
+var WebSocketServer = require('websocket-server').Server;
 
 var PORT = 1024 + Math.floor(Math.random() * 4096);
 
